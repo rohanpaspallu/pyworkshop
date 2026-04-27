ed13a85366f0c1488bf141bca69938859f1650d1
ROHAN DEEPAK PASPALLU <paspallu.rohan@gmail.com>
2026-04-27 14:11:17 -0400

    implemented some more list comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -8,3 +8,13 @@ print(even_length_names)
 
 upper_case_names = [name.upper() for name in names]
 print(upper_case_names)
+
+# getting range of numbers using list comprehension
+
+print(list(range(10)))  # This will create a list of numbers from 0 to 9
+print(list(range(1, 11)))  # This will create a list of numbers from 1 to 10
+print(list(range(0, 20, 2)))  # This will create a list of even numbers from 0 to 18
+
+# get squares of numbers from 0 to 9 using list comprehension
+squares = [x**2 for x in range(10)]
+print(squares)
